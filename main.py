09c0cf6881d5f1b166190ed91003692fd51a6b71
Efim2003 <fima.rubin@gmail.com>
2025-11-29 20:12:09 +0200

    order

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # This line is first to load environment variables
 import config
 import logging
-from bot.core import run_bot
-from db.core import init_db
-# Configure logging
+
+# Configure logging immediately, before importing other modules that might log at module level
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
+from bot.core import run_bot
+from db.core import init_db
 
 if __name__ == '__main__':
     init_db()
