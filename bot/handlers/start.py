ccf3d9521f14379ce105ecbffe3509d26df45ebe
Efim2003 <fima.rubin@gmail.com>
2025-11-29 20:45:52 +0200

    logger

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,8 +1,12 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+import logging
+
+logger = logging.getLogger(__name__)
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
     Responds to /start command.
     """
+    logger.info("User %s started the bot", update.effective_user.id or "Unknown")
     await update.message.reply_text("Вітаю, Юначе!")
